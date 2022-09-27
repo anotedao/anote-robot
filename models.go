2490d9610ca590149bf05e9441196b3f928dec38
Pragmatic <cryptopragmatic@protonmail.com>
2022-09-27 08:12:19 +0200

    telegram notifications

--- models.go
+++ models.go
@@ -1,9 +1,13 @@
 package main
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Miner struct {
 	gorm.Model
-	Address         string `gorm:"size:255;uniqueIndex"`
-	NotificationDay uint64
+	Address          string `gorm:"size:255;uniqueIndex"`
+	LastNotification time.Time
 }
--- monitor.go
+++ monitor.go
@@ -69,8 +69,8 @@ func (m *Monitor) isSending(miner proto.DataEntry) bool {
 
 	// log.Println(prettyPrint(dbminer))
 
-	if (int64(height)-minerHeight.(int64)) > 1440 && dbminer.NotificationDay != uint64(time.Now().Day()) {
-		dbminer.NotificationDay = uint64(time.Now().Day())
+	if (int64(height)-minerHeight.(int64)) > 1440 && (int64(height)-minerHeight.(int64)) < 2880 && time.Since(dbminer.LastNotification) > time.Hour*24 {
+		dbminer.LastNotification = time.Now()
 		db.Save(dbminer)
 
 		return true
