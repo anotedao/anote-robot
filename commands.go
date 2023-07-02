b5df22a29877ef34e2e6cacf05ac0e1478ebb214
Pragmatic <cryptopragmatic@protonmail.com>
2023-07-03 00:54:15 +0200

    alpha command

--- commands.go
+++ commands.go
@@ -447,7 +447,7 @@ func alphaCommand(c telebot.Context) error {
 	miner := getMiner(c.Message().Sender.ID)
 	height := getHeight()
 
-	if height <= 250000 {
+	if height <= 314000 {
 		if strings.HasPrefix(miner.Address, "3A") {
 			alp := &Alpha{}
 			db.First(alp, &Alpha{Address: miner.Address})
