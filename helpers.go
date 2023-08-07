2d051f4d01b220d61d4e8257de12df712f485066
Pragmatic <cryptopragmatic@protonmail.com>
2023-08-07 18:22:36 +0200

    bigger timeout

--- helpers.go
+++ helpers.go
@@ -386,7 +386,7 @@ func getCallerInfo() (info string) {
 
 func getStats() (*StatsResponse, error) {
 	client := http.Client{
-		Timeout: 15 * time.Second,
+		Timeout: 30 * time.Second,
 	}
 
 	resp, err := client.Get("http://localhost:5001/stats")
