d99c4762f48893151a8580c93c98550602295217
Pragmatic <cryptopragmatic@protonmail.com>
2023-08-09 21:05:41 +0200

    bsc command

--- commands.go
+++ commands.go
@@ -374,7 +374,12 @@ func addressBscCommand(c telebot.Context) error {
 
 	help := "Address of Anote contract in BSC chain:\n\n`0xbad04e33cc88bbcccc1b7adb8319f7d36f5bc472`"
 
-	_, err := bot2.Send(m.Chat, help, telebot.NoPreview)
+	o := telebot.SendOptions{
+		ParseMode:             "MarkdownV2",
+		DisableWebPagePreview: true,
+	}
+
+	_, err := bot2.Send(m.Chat, help, o)
 
 	return err
 }
--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/go-macaron/captcha v0.2.0
 	github.com/wavesplatform/gowaves v0.10.1
 	gopkg.in/macaron.v1 v1.4.0
-	gopkg.in/telebot.v3 v3.0.0
+	gopkg.in/telebot.v3 v3.1.3
 	gorm.io/driver/sqlite v1.3.6
 	gorm.io/gorm v1.23.10
 )
