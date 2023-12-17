ad8f881dce2a0f95897d52b6afb2e220341084a6
Pragmatic <cryptopragmatic@protonmail.com>
2023-12-17 22:30:54 +0100

    node mint notification

--- monitor.go
+++ monitor.go
@@ -18,6 +18,7 @@ type Monitor struct {
 	NodeBalance        uint64
 	AintBalance        uint64
 	AintPrice          float64
+	NodePrice          float64
 }
 
 func (m *Monitor) monitorAintBuys() {
@@ -101,10 +102,6 @@ func (m *Monitor) monitorAintBuys() {
 func (m *Monitor) monitorNodeMints() {
 	count := 0
 
-	ga := GATEWAY_ADDR
-	nodePrice, _ := getData("%s__nodePrice", &ga)
-	logTelegram(prettyPrint(nodePrice))
-
 	asset, err := crypto.NewDigestFromBase58(NodeAnoteId)
 	if err != nil {
 		log.Println(err)
@@ -136,31 +133,33 @@ func (m *Monitor) monitorNodeMints() {
 
 		if nnodes > 0 && count > 0 {
 			notificationTelegram(fmt.Sprintf("<u><strong>New NODE Minted!</strong></u> 🚀\n\n%d NODE\n\nAbout NODE Token:\nanotedao.com/node-token", nnodes))
-			// notificationTelegramTeam(fmt.Sprintf("<u><strong>New AINT Minted!</strong></u> 🚀\n\nPaid:\n%.8f ANOTE ($%.2f)\nMinted:\n%.8f AINT", nb, usd, naints))
-			// notificationTelegramGroup(fmt.Sprintf("<u><strong>New NODE Minted!</strong></u> 🚀\n\n%d NODEs", nnodes))
+			notificationTelegramTeam(fmt.Sprintf("<u><strong>New NODE Minted!</strong></u> 🚀\n\n%d NODE\n\nAbout NODE Token:\nanotedao.com/node-token", nnodes))
+			notificationTelegramGroup(fmt.Sprintf("<u><strong>New NODE Minted!</strong></u> 🚀\n\n%d NODE\n\nAbout NODE Token:\nanotedao.com/node-token", nnodes))
 		}
 
-		// if count > 0 {
-		// 	ap, err := getData2("%s__price", nil)
-		// 	if err != nil {
-		// 		log.Println(err)
-		// 		logTelegram(err.Error())
-		// 	}
+		if count > 0 {
+			ga := GATEWAY_ADDR
+			np, err := getData("%s__nodePrice", &ga)
+			if err != nil {
+				log.Println(err)
+				logTelegram(err.Error())
+			}
 
-		// 	apf := float64(ap.(int64)) / MULTI8
+			npf := float64(np.(int64)) / 100
 
-		// 	if apf > m.AintPrice {
-		// 		notificationTelegramTeamPin(fmt.Sprintf("<u><strong>AINT Price Increased!</strong></u> 🚀\n\nNew Price:\n$%.2f", apf))
-		// 		notificationTelegramGroupPin(fmt.Sprintf("<u><strong>AINT Price Increased!</strong></u> 🚀\n\nNew Price:\n$%.2f", apf))
-		// 	}
-		// }
+			if npf > m.NodePrice {
+				notificationTelegramTeamPin(fmt.Sprintf("<u><strong>NODE Price Increased!</strong></u> 🚀\n\nNew Price:\n$%.2f BNB", npf))
+				notificationTelegramGroupPin(fmt.Sprintf("<u><strong>NODE Price Increased!</strong></u> 🚀\n\nNew Price:\n$%.2f BNB", npf))
+			}
+		}
 
-		// ap, err := getData2("%s__price", nil)
-		// if err != nil {
-		// 	log.Println(err)
-		// 	logTelegram(err.Error())
-		// }
-		// m.AintPrice = float64(ap.(int64)) / MULTI8
+		ga := GATEWAY_ADDR
+		np, err := getData("%s__nodePrice", &ga)
+		if err != nil {
+			log.Println(err)
+			logTelegram(err.Error())
+		}
+		m.NodePrice = float64(np.(int64)) / 100
 
 		cancel()
 
