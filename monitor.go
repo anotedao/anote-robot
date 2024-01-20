c8b655ddeda27a67e32e491e169789110c2ddca2
Pragmatic <cryptopragmatic@protonmail.com>
2024-01-20 03:32:34 +0100

    don't spam group with forward

--- monitor.go
+++ monitor.go
@@ -206,12 +206,16 @@ func (m *Monitor) forwardCompetition() {
 		msg.Chat = ch
 
 		log.Println(prettyPrint(msg))
-		log.Println(prettyPrint(m1.Text))
-		log.Println(prettyPrint(m2.Text))
-		log.Println(prettyPrint(m3.Text))
-		log.Println(prettyPrint(m1.OriginalMessageID))
-		log.Println(prettyPrint(m2.OriginalMessageID))
-		log.Println(prettyPrint(m3.OriginalMessageID))
+		if m1 != nil &&
+			m2 != nil &&
+			m3 != nil {
+			log.Println(prettyPrint(m1.Text))
+			log.Println(prettyPrint(m2.Text))
+			log.Println(prettyPrint(m3.Text))
+			log.Println(prettyPrint(m1.OriginalMessageID))
+			log.Println(prettyPrint(m2.OriginalMessageID))
+			log.Println(prettyPrint(m3.OriginalMessageID))
+		}
 
 		if m1 != nil &&
 			m2 != nil &&
