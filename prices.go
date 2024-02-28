3525324d7ddca9c4ce1c985184e22a435a49e7a9
Pragmatic <cryptopragmatic@protonmail.com>
2024-02-28 19:02:31 +0100

    price update

--- prices.go
+++ prices.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -63,7 +64,7 @@ func (pc *PriceClient) doRequest() (*Prices, error) {
 }
 
 func (pc *PriceClient) loadPrice() {
-	pc.AnotePrice = getPriceDexTools()
+	pc.AnotePrice = getPriceCoinGecko()
 }
 
 func (pc *PriceClient) doRequestOrderbook() {
@@ -261,81 +262,81 @@ type CoinGeckoResponse struct {
 }
 
 func getPriceCoinGecko() float64 {
-	price := float64(1.5)
+	price := float64(50)
 
-	// cgr := &CoinGeckoResponse{}
-	// cl := http.Client{}
+	cgr := &CoinGeckoResponse{}
+	cl := http.Client{}
 
-	// var req *http.Request
-	// var err error
+	var req *http.Request
+	var err error
 
-	// req, err = http.NewRequest(http.MethodGet, CoinGeckoURL, nil)
+	req, err = http.NewRequest(http.MethodGet, CoinGeckoURL, nil)
 
-	// req.Header.Set("Content-Type", "application/json;version=20230302")
-	// req.Header.Set("Accept", "application/json;version=20230302")
-	// req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
+	req.Header.Set("Content-Type", "application/json;version=20230302")
+	req.Header.Set("Accept", "application/json;version=20230302")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36")
 
-	// log.Println(prettyPrint(req))
+	log.Println(prettyPrint(req))
 
-	// if err != nil {
-	// 	log.Println(err)
-	// 	logTelegram(err.Error())
-	// 	return price
-	// }
+	if err != nil {
+		log.Println(err)
+		logTelegram(err.Error())
+		return price
+	}
 
-	// res, err := cl.Do(req)
+	res, err := cl.Do(req)
 
-	// log.Println(prettyPrint(res))
+	log.Println(prettyPrint(res))
 
-	// if err == nil {
-	// 	log.Println(prettyPrint(res.Body))
-	// 	body, err := ioutil.ReadAll(res.Body)
-	// 	if err != nil {
-	// 		log.Println(err)
-	// 		logTelegram(err.Error())
-	// 		return price
-	// 	}
-	// 	if res.StatusCode != 200 && res.StatusCode != 304 {
-	// 		err := errors.New(res.Status)
-	// 		log.Println(err)
-	// 		log.Println(res.Body)
-	// 		logTelegram(err.Error())
-	// 		return price
-	// 	}
-	// 	json.Unmarshal(body, cgr)
-	// } else {
-	// 	log.Println(err)
-	// 	logTelegram(err.Error())
-	// 	return price
-	// }
+	if err == nil {
+		log.Println(prettyPrint(res.Body))
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Println(err)
+			logTelegram(err.Error())
+			return price
+		}
+		if res.StatusCode != 200 && res.StatusCode != 304 {
+			err := errors.New(res.Status)
+			log.Println(err)
+			log.Println(res.Body)
+			logTelegram(err.Error())
+			return price
+		}
+		json.Unmarshal(body, cgr)
+	} else {
+		log.Println(err)
+		logTelegram(err.Error())
+		return price
+	}
 
-	// fdv, err := strconv.ParseFloat(cgr.Data.Attributes.FdvUsd, 32)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	logTelegram(err.Error())
-	// 	return price
-	// }
+	fdv, err := strconv.ParseFloat(cgr.Data.Attributes.FdvUsd, 32)
+	if err != nil {
+		log.Println(err)
+		logTelegram(err.Error())
+		return price
+	}
 
-	// ts, err := strconv.ParseFloat(cgr.Data.Attributes.TotalSupply, 32)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	logTelegram(err.Error())
-	// 	return price
-	// }
+	ts, err := strconv.ParseFloat(cgr.Data.Attributes.TotalSupply, 32)
+	if err != nil {
+		log.Println(err)
+		logTelegram(err.Error())
+		return price
+	}
 
-	// prc, err := strconv.ParseFloat(cgr.Data.Attributes.PriceUsd, 32)
-	// if err != nil {
-	// 	log.Println(err)
-	// 	logTelegram(err.Error())
-	// 	return price
-	// }
+	prc, err := strconv.ParseFloat(cgr.Data.Attributes.PriceUsd, 32)
+	if err != nil {
+		log.Println(err)
+		logTelegram(err.Error())
+		return price
+	}
 
-	// ts = ts / float64(MULTI8)
+	ts = ts / float64(MULTI8)
 
-	// price = fdv / ts
+	price = fdv / ts
 
-	price = 50.47
-	prc := price
+	// price = 50.47
+	// prc := price
 
 	priceInt := int64(prc * MULTI8)
 
